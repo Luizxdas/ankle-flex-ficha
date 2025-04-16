c465edfc775929380914d74706c3f6411f521746
Luizxdas <luizxdas@outlook.com>
2025-04-16 07:53:48 -0300

    Arruma pequeno erro visual onde existiam linhas duplicadas

--- formulario/src/utils.jsx
+++ formulario/src/utils.jsx
@@ -1,7 +1,7 @@
 export const inputStyle = "h-[1.5em] ml-[4px] uppercase";
 export const vFormStyle = "w-[15em] border-[1.5px] border-black rounded-md";
 export const baseFormStyle =
-  "h-[31em] rounded-md select-none text-start divide-y divide-black";
+  "h-[31em] rounded-md select-none text-start divide-y";
 
 export const preencherFormulario = (dados, formRef) => {
   if (!formRef?.current) return;
