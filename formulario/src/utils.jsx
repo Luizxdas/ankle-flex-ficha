cf4d5bde8b882d0569fcda79124204bbd0f52735
Luizxdas <luizxdas@outlook.com>
2025-04-17 16:58:57 -0300

    Move funções de utilidade para utils.jsx para uso global

--- formulario/src/utils.jsx
+++ formulario/src/utils.jsx
@@ -4,16 +4,74 @@ export const baseFormStyle =
   "h-[31em] rounded-md select-none text-start divide-y";
 
 export const preencherFormulario = (dados, formRef) => {
-  if (!formRef?.current) return;
+  if (!formRef?.current || !dados) return;
 
-  const dadosParaPreencher = dados.data;
-
-  Object.entries(dadosParaPreencher).forEach(([key, value]) => {
+  Object.entries(dados).forEach(([key, value]) => {
     const input = formRef.current.querySelector(`#${key}`);
     if (input) {
       input.value = value;
     } else {
-      console.log("Input não encontrado de id: ", key);
+      console.log("Input não encontrado de id:", key);
     }
+  });
+};
+
+export const checarObrigatorio = (lado) => {
+  if (lado === "frente") {
+    const n_ficha_paciente = document.getElementById("n_ficha_paciente");
+    const nome_paciente = document.getElementById("nome_paciente");
+    const data_ficha = document.getElementById("data_ficha");
+
+    if (
+      !n_ficha_paciente.value.trim() ||
+      !nome_paciente.value.trim() ||
+      !data_ficha.value.trim()
+    ) {
+      alert(
+        "Os campos de número da ficha, nome do paciente e data da ficha precisam estar todos preenchidos!"
+      );
+      return false;
+    }
+
+    return true;
+  } else if (lado === "verso") {
+    const n_ficha_paciente = document.getElementById("n_ficha_paciente");
+    if (!n_ficha_paciente.value.trim()) {
+      alert("O campo de número da ficha precisa estar preenchido!");
+      return false;
+    }
+
+    return true;
+  }
+};
+
+export const salvarFicha = (formRef, lado) => {
+  if (checarObrigatorio(lado)) {
+    const formData = new FormData(formRef.current);
+    const dados = Object.fromEntries(formData.entries());
+
+    sessionStorage.setItem(
+      `${lado === "frente" ? "formFrente" : "formVerso"}`,
+      JSON.stringify(dados)
+    );
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const limparFicha = (formRef, lado) => {
+  if (!formRef?.current || !lado) {
+    console.error("formRef ou lado não definidos: ");
+    return;
+  }
+
+  lado === "frente"
+    ? sessionStorage.removeItem("formFrente")
+    : sessionStorage.removeItem("formVerso");
+
+  const inputs = formRef.current.querySelectorAll("input");
+  inputs.forEach((input) => {
+    input.value = "";
   });
 };
