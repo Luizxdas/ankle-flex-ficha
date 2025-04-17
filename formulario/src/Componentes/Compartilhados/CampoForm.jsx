e186742b5aa2c0a73f601f1973fc65c001a0c205
Luizxdas <luizxdas@outlook.com>
2025-04-17 17:02:17 -0300

    Adiciona campo name ao input

--- formulario/src/Componentes/Compartilhados/CampoForm.jsx
+++ formulario/src/Componentes/Compartilhados/CampoForm.jsx
@@ -1,11 +1,16 @@
 import { inputStyle } from "../../utils";
 
-function CampoForm({ content, id, width, border }) {
+function CampoForm({ content, id, name, width, border }) {
   return (
     <div className={`${border ? "border-black border-b-[1.5px]" : ""}`}>
       <label htmlFor={id} className="mx-2">
         {content}
-        <input id={id} className={`${inputStyle}`} style={{ width }} />
+        <input
+          id={id}
+          name={name}
+          className={`${inputStyle}`}
+          style={{ width }}
+        />
       </label>
     </div>
   );
