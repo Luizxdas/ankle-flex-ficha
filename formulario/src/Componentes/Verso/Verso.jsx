fa200f3d0461d9568dbe96257736329c93292c54
Luizxdas <luizxdas@outlook.com>
2025-04-20 11:19:00 -0300

    Adiciona campo de nome aos inputs e botão de limpar ficha e mudanças menores
    -Troca botão "ir para frente" para "voltar"
    -Move botão imprimir e salvar por estilo

--- formulario/src/Componentes/Verso/Verso.jsx
+++ formulario/src/Componentes/Verso/Verso.jsx
@@ -1,38 +1,34 @@
-import { useRef } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import useVersoForm from "./Hooks/useVersoForm";
 import CampoForm from "../Compartilhados/CampoForm";
 import VersoBaseForm from "./VersoBaseForm";
 import Botao from "../Compartilhados/Botao";
-import { inputStyle } from "../../utils";
+import { inputStyle, limparFicha, salvarFicha } from "../../utils";
+import { enviarDados } from "./Utils/versoUtils";
 
-function Verso() {
-  const pacienteRef = useRef(null);
+function Verso({ mudarPagina }) {
   const formRef = useRef(null);
-  const location = useLocation();
-  const { db, pagina } = useVersoForm(pacienteRef, location.pathname);
+  const [ficha, setFicha] = useState("");
+  const { pagina } = useVersoForm(formRef, setFicha);
+
+  const handleVoltar = () => {
+    if (salvarFicha(formRef, "verso")) {
+      mudarPagina();
+    }
+  };
 
   return (
     <div className="h-screen w-screen bg-slate-400 flex justify-center items-center flex-row">
       {/*  BOTÕES */}
-      <div className="relative bottom-[13em] mr-3 flex flex-col justify-center space-y-4 print:hidden">
+      <div className="relative bottom-[19em] mr-3 flex flex-col justify-center space-y-4 print:hidden">
         <div>
-          <Link to={"/pacientes"}>
-            <Botao conteudo={"Buscar Pacientes"} />
-          </Link>
-        </div>
-        <div>
-          <Link to={"/"}>
-            <Botao conteudo={"Ir para a frente"} />
-          </Link>
-        </div>
-        <div>
-          <Botao conteudo={"Imprimir"} onClick={pagina.imprimir} />
+          <Botao conteudo={"Voltar"} onClick={handleVoltar} />
         </div>
         <div>
           <Botao
-            conteudo={"Salvar"}
-            onClick={() => db.salvarPaciente(pacienteRef, formRef)}
+            conteudo={"Limpar ficha"}
+            onClick={() => limparFicha(formRef, "verso")}
           />
         </div>
       </div>
@@ -58,40 +54,52 @@ function Verso() {
                       <div className="flex justify-between">
                         <CampoForm
                           id={"idade"}
+                          name={"idade"}
                           content={"IDADE:"}
                           width={"5em"}
                         />
                         <CampoForm
                           id={"sexo"}
+                          name={"sexo"}
                           content={"SEXO:"}
                           width={"5em"}
                         />
                         <CampoForm
                           id={"altura"}
+                          name={"altura"}
                           content={"ALTURA:"}
                           width={"5em"}
                         />
                         <CampoForm
                           id={"peso"}
+                          name={"peso"}
                           content={"PESO:"}
                           width={"5em"}
                         />
                         <CampoForm
                           id={"lado"}
+                          name={"lado"}
                           content={"LADO:"}
                           width={"5em"}
                         />
                       </div>
                     </div>
                     <div className="py-2 flex justify-between">
-                      <CampoForm id={"n_pe"} content={"Nº PÉ:"} width={"3em"} />
                       <CampoForm
-                        id={"causa"}
+                        id={"n_pe"}
+                        name={"n_pe"}
+                        content={"Nº PÉ:"}
+                        width={"3em"}
+                      />
+                      <CampoForm
+                        id={"causa_amputacao"}
+                        name={"causa_amputacao"}
                         content={"CAUSA DA AMPUTAÇÃO:"}
                         width={"15em"}
                       />
                       <CampoForm
                         id={"tempo"}
+                        name={"tempo"}
                         content={"TEMPO:"}
                         width={"6em"}
                       />
@@ -104,9 +112,11 @@ function Verso() {
                       </label>
                       <input
                         id="n_ficha_paciente"
+                        name="n_ficha_paciente"
                         className={`w-[6.5em] ${inputStyle}`}
                         maxLength={16}
-                        ref={pacienteRef}
+                        value={ficha}
+                        onChange={(e) => setFicha(e.target.value)}
                       />
                     </div>
                   </div>
@@ -125,6 +135,18 @@ function Verso() {
               </div>
             </form>
           </div>
+        </div>
+      </div>
+
+      <div className="relative bottom-[19em] ml-3 flex flex-col justify-center space-y-4 print:hidden">
+        <div>
+          <Botao conteudo={"Imprimir"} onClick={pagina.imprimir} />
+        </div>
+        <div>
+          <Botao
+            conteudo={"Salvar"}
+            onClick={() => enviarDados(formRef, "salvar")}
+          />
         </div>
       </div>
     </div>
