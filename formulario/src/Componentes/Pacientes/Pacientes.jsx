ab4781d0b99b6db06e73c0b1df0b42dc8bf191c4
Luizxdas <luizxdas@outlook.com>
2025-04-19 23:33:14 -0300

    Armazena os dados da ficha no sessionStorage ao invés de apenas o número

--- formulario/src/Componentes/Pacientes/Pacientes.jsx
+++ formulario/src/Componentes/Pacientes/Pacientes.jsx
@@ -3,15 +3,24 @@ import Pesquisa from "./Pesquisa";
 import Botao from "../Compartilhados/Botao";
 import ListaPacientes from "./ListaPacientes";
 import { useState } from "react";
+import { buscarDadosPaciente } from "../../api/api";
 
 function Pacientes() {
   const navigate = useNavigate();
   const [ficha, setFicha] = useState(null);
 
-  const handleAtualizar = () => {
+  const handleAtualizar = async () => {
     if (ficha) {
-      sessionStorage.setItem("n_ficha_paciente", ficha);
-      navigate("/");
+      const dados = await buscarDadosPaciente(ficha);
+      sessionStorage.setItem(
+        "formFrente",
+        JSON.stringify(dados.data.dadosFrente)
+      );
+      sessionStorage.setItem(
+        "formVerso",
+        JSON.stringify(dados.data.dadosVerso)
+      );
+      navigate("/ficha?lado=frente");
     } else {
       alert("Nenhuma ficha selecionada! Clique para selecionar um ficha.");
     }
@@ -20,11 +29,8 @@ function Pacientes() {
   return (
     <div className="h-screen w-screen bg-slate-400 flex justify-center items-center flex-row">
       <div className="relative bottom-[14em] left-16 flex flex-col justify-center space-y-4">
-        <Link to={"/verso"}>
-          <Botao conteudo={"Ir para o verso"} />
-        </Link>
         <Link to={"/"}>
-          <Botao conteudo={"Ir para a frente"} />
+          <Botao conteudo={"Voltar"} />
         </Link>
       </div>
 
