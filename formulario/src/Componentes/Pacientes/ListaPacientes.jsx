53da416e3b4d4e73dcd3e9f5a08cb3c8a6992a8f
Luizxdas <luizxdas@outlook.com>
2025-05-13 01:51:18 -0300

    Adiciona tratamento de erros e tela de loading

--- formulario/src/Componentes/Pacientes/ListaPacientes.jsx
+++ formulario/src/Componentes/Pacientes/ListaPacientes.jsx
@@ -6,6 +6,8 @@ import CabecalhoLista from "./CabecalhoLista";
 function ListaPacientes({ setNFicha }) {
   const [dados, setDados] = useState([]);
   const [ativo, setAtivo] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleClick = (item) => {
     if (ativo === item) {
@@ -20,10 +22,14 @@ function ListaPacientes({ setNFicha }) {
   useEffect(() => {
     const carregarDados = async () => {
       try {
+        setLoading(true);
         const resposta = await buscarTodosDados();
         setDados(resposta.dados);
+        setLoading(false);
       } catch (error) {
         console.error("Erro ao carregar dados:", error);
+        setLoading(false);
+        setError(true);
       }
     };
 
@@ -33,6 +39,22 @@ function ListaPacientes({ setNFicha }) {
   return (
     <div className="w-[60em] h-[45em] flex flex-col p-[6px] bg-gray-100">
       <CabecalhoLista />
+      {isLoading && (
+        <div className="w-full h-full flex justify-center items-center ">
+          <div className="flex items-center justify-center space-x-2">
+            <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce [animation-delay:-0.3s]"></span>
+            <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce [animation-delay:-0.15s]"></span>
+            <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce"></span>
+          </div>
+        </div>
+      )}
+      {error && (
+        <div className="w-full h-full flex justify-center items-center bg-slate-900/55">
+          <div className="text-3xl bg-slate-400 w-[15em] h-[8em] flex justify-center rounded-md shadow-lg">
+            <span className="mt-12">Erro ao carregar os dados!</span>
+          </div>
+        </div>
+      )}
       <div className="flex flex-col gap-2 mt-2">
         {dados.map((item, index) => (
           <DadosLista
