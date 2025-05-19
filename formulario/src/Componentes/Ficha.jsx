804abd4dc5cddef9ed48d3cf766106c550b314d9
Luizxdas <luizxdas@outlook.com>
2025-05-19 14:42:46 -0300

    Adiciona modal para mensagens de erro, sucesso e alerta substituindo o componente Alerta

--- formulario/src/Componentes/Ficha.jsx
+++ formulario/src/Componentes/Ficha.jsx
@@ -3,58 +3,76 @@ import Frente from "./Frente/Frente";
 import Verso from "./Verso/Verso";
 import Botao from "./Compartilhados/Botao";
 import useFicha from "../hooks/useFicha";
-import Alerta from "./Alerta";
+import Modal from "./Compartilhados/Modal";
 
 function Ficha() {
   const frenteRef = useRef();
   const versoRef = useRef();
   const operacao = sessionStorage.getItem("operacao");
-  const { pagina } = useFicha(frenteRef, versoRef);
+  const [modal, setModal] = useState("");
   const [nFicha, setNFicha] = useState("");
-  const [alerta, setAlerta] = useState(false);
+  const { pagina } = useFicha(frenteRef, versoRef, setModal, setNFicha);
+
+  const handleSubmit = () => {
+    pagina.handleSalvar();
+  };
+
+  const onModalClose = () => {
+    setModal("");
+    pagina.setError(false);
+  };
+
+  const onModalConfirm = () => {
+    pagina.enviarParaServidor();
+  };
 
   return (
-    <div className=" bg-slate-400 flex flex-row items-center justify-center">
-      {alerta ? (
-        <Alerta
-          setAlerta={setAlerta}
-          onConfirmar={() => pagina.enviarParaServidor()}
-        />
-      ) : (
-        ""
+    <div>
+      {modal && (
+        <div className="fixed w-screen h-screen bg-black/40 flex items-center justify-center z-10">
+          <Modal
+            tipo={modal}
+            onClose={onModalClose}
+            onConfirm={onModalConfirm}
+            isLoading={pagina.isLoading}
+            erro={pagina.error}
+          />
+        </div>
       )}
-      <div className="print:hidden relative right-44">
-        <div className="fixed top-10">
-          <div className="flex flex-col space-y-4">
-            <Botao conteudo={"Voltar"} onClick={pagina.handleVoltar} />
+      <div className=" bg-slate-400 flex flex-row items-center justify-center z-0">
+        <div className="print:hidden relative right-44">
+          <div className="fixed top-10">
+            <div className="flex flex-col space-y-4">
+              <Botao conteudo={"Voltar"} onClick={pagina.handleVoltar} />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="flex flex-col">
-        <div className="print:hidden mt-8"></div>
-        <div className="w-[297mm] h-[200mm]">
-          <Frente frenteRef={frenteRef} nFicha={nFicha} setNFicha={setNFicha} />
-        </div>
-        <div className="print:hidden my-12"></div>
-        <div className="page-break w-[297mm] h-[200mm]">
-          <Verso versoRef={versoRef} nFicha={nFicha} setNFicha={setNFicha} />
-        </div>
-        <div className="print:hidden mb-12"></div>
-      </div>
-
-      <div className="print:hidden relative">
-        <div className="ml-8 fixed top-10">
-          <div className="flex flex-col space-y-4">
-            <Botao
-              conteudo={operacao === "atualizar" ? "Atualizar" : "Salvar"}
-              onClick={() =>
-                operacao === "atualizar"
-                  ? pagina.handleAtualizar(setAlerta)
-                  : pagina.handleSalvar(setAlerta)
-              }
+        <div className="flex flex-col">
+          <div className="print:hidden mt-8"></div>
+          <div className="w-[297mm] h-[200mm]">
+            <Frente
+              frenteRef={frenteRef}
+              nFicha={nFicha}
+              setNFicha={setNFicha}
             />
-            <Botao conteudo={"Imprimir"} onClick={() => window.print()} />
+          </div>
+          <div className="print:hidden my-12"></div>
+          <div className="page-break w-[297mm] h-[200mm]">
+            <Verso versoRef={versoRef} nFicha={nFicha} setNFicha={setNFicha} />
+          </div>
+          <div className="print:hidden mb-12"></div>
+        </div>
+
+        <div className="print:hidden relative">
+          <div className="ml-8 fixed top-10">
+            <div className="flex flex-col space-y-4">
+              <Botao
+                conteudo={operacao === "atualizar" ? "Atualizar" : "Salvar"}
+                onClick={handleSubmit}
+              />
+              <Botao conteudo={"Imprimir"} onClick={() => window.print()} />
+            </div>
           </div>
         </div>
       </div>
