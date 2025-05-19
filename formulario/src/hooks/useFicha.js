728256229c8d81a40ff245333bd3029af3d39c19
Luizxdas <luizxdas@outlook.com>
2025-05-19 14:23:09 -0300

    Adiciona um melhor tratamento de erros e gerencia número da ficha

--- formulario/src/hooks/useFicha.js
+++ formulario/src/hooks/useFicha.js
@@ -3,30 +3,19 @@ import { checarObrigatorio, preencherFormulario } from "../utils";
 import { atualizarDados, enviarDados } from "../api/api";
 import { useNavigate } from "react-router-dom";
 
-const useFicha = (frenteRef, versoRef) => {
+const useFicha = (frenteRef, versoRef, setModal, setNFicha) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
-  const handleSalvar = (setAlerta) => {
-    salvarLocalmente();
+  const handleSalvar = () => {
     if (checarObrigatorio() === "n_ficha") {
-      alert("Preencha o campo Nº FICHA!");
-    } else if (checarObrigatorio() === true) {
-      enviarParaServidor("salvar");
+      setModal("n_ficha");
+    } else if (checarObrigatorio()) {
+      setModal(true);
+      enviarParaServidor();
     } else {
-      setAlerta(true);
-    }
-  };
-
-  const handleAtualizar = (setAlerta) => {
-    salvarLocalmente();
-    if (checarObrigatorio() === "n_ficha") {
-      alert("Preencha o campo Nº FICHA!");
-    } else if (checarObrigatorio() === true) {
-      enviarParaServidor("atualizar");
-    } else {
-      setAlerta(true);
+      setModal("dados_faltando");
     }
   };
 
@@ -108,13 +97,24 @@ const useFicha = (frenteRef, versoRef) => {
     }
   };
 
-  const enviarParaServidor = (operacao) => {
+  const enviarParaServidor = async () => {
+    setModal(true);
+    salvarLocalmente();
     const dados = JSON.parse(sessionStorage.getItem("dados"));
+    const operacao = sessionStorage.getItem("operacao");
+    setIsLoading(true);
 
-    if (operacao === "salvar") {
-      enviarDados(dados);
-    } else if (operacao === "atualizar") {
-      atualizarDados(dados);
+    try {
+      if (operacao === "salvar") {
+        await enviarDados(dados);
+      } else if (operacao === "atualizar") {
+        await atualizarDados(dados);
+      }
+    } catch (error) {
+      setError(error.message || error);
+      console.error("Erro durante envio:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -122,17 +122,18 @@ const useFicha = (frenteRef, versoRef) => {
     const dados = JSON.parse(sessionStorage.getItem("dados")) || "";
     if (dados) {
       preencherFormulario(dados, frenteRef, versoRef);
+      setNFicha(dados.identidade.N_FICHA);
     }
-  }, [frenteRef, versoRef]);
+  }, [frenteRef, setNFicha, versoRef]);
 
   return {
     pagina: {
       handleSalvar,
-      handleAtualizar,
       handleVoltar,
       enviarParaServidor,
       isLoading,
       error,
+      setError,
     },
   };
 };
