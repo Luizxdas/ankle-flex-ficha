d89d56708a029f5944a0065cccc6ba467cd6e6be
Luizxdas <luizxdas@outlook.com>
2025-07-15 15:24:10 -0300

    Remove código redundante, reseta formulário ao ir em criar nova ficha e alterações menores

--- Web/src/hooks/useFichav2.js
+++ Web/src/hooks/useFichav2.js
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
-import { atualizarDados, enviarDados } from "../api/api";
+import { enviarDados } from "../api/api";
 import { useNavigate } from "react-router-dom";
 import { buscarDadosFicha } from "../api/api";
+import dadosForm from "../Utils/dadosForm";
 
-const useFichav2 = (formRef, setModal, formData) => {
+const useFichav2 = (formRef, setModal, formData, setFormData) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [listaId, setListaId] = useState({});
-  const [dadosForm, setDadosForm] = useState({});
   const navigate = useNavigate();
 
   const handleSalvar = async () => {
@@ -15,17 +14,9 @@ const useFichav2 = (formRef, setModal, formData) => {
     setModal(true);
 
     const dadosFormatados = formatarDados(formData);
-    const operacao = sessionStorage.getItem("operacao");
 
     try {
-      if (operacao === "salvar") {
-        console.log(dadosFormatados);
-        await enviarDados(dadosFormatados);
-      } else if (operacao === "atualizar") {
-        console.log(dadosFormatados);
-
-        // await atualizarDados(dadosFormatados);
-      }
+      await enviarDados(dadosFormatados);
     } catch (error) {
       console.error("Erro durante a operação:", error.message);
       setError(error.message);
@@ -48,29 +39,26 @@ const useFichav2 = (formRef, setModal, formData) => {
   };
 
   function formatarDados(dados) {
-    const tabelas = { ...dadosForm };
+    const tabelas = {};
     tabelas["produtos"] = [];
 
     for (const [id, obj] of Object.entries(dados)) {
-      console.log(id, obj.valor, obj.tabela);
-
-      if (obj.valor && obj.tabela) {
+      if (obj.valor) {
         if (obj.tabela === "produtos") {
           const item = { produto: id, tipo: obj.tipo };
           tabelas["produtos"].push(item);
+        } else if (id === "ficha_id") {
+          tabelas["identidade"][id] = obj.valor;
         } else {
           if (!tabelas[obj.tabela]) {
             tabelas[obj.tabela] = {};
           }
           tabelas[obj.tabela][id] = obj.valor;
         }
-      } else {
-        console.warn(
-          `Campo '${id}' não pôde ser atribuído a uma tabela válida.`
-        );
       }
     }
 
+    console.log(tabelas);
     return tabelas;
   }
 
@@ -78,70 +66,46 @@ const useFichav2 = (formRef, setModal, formData) => {
     async function preencherFicha(ficha_link) {
       setIsLoading(true);
       const dados = await buscarDadosFicha(ficha_link);
+      setIsLoading(false);
 
-      if (dados) {
-        setDadosForm(dados);
-      } else {
+      if (!dados) {
         console.warn("Dados inválidos para preenchimento do formulário");
+
         return;
       }
 
-      console.log("Dados recebidos para preenchimento: ", dados);
-
+      const novoFormData = formData;
       const tabelasId = {};
 
-      const getInput = (id) => formRef.current?.querySelector(`#${id}`);
-      const getProdutoInput = (nome) =>
-        formRef.current?.querySelector(`input[value="${nome}"]`);
-
-      const preencherValor = (input, valor) => {
-        if (!input) {
-          console.warn("Input ", input, " não encontrado para o valor:", valor);
-          return false;
-        }
-
-        if (input.type === "checkbox") {
-          input.checked = true;
-        } else if (input.id === "informacoes_preco") {
-          input.value = new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          }).format(valor);
-        } else {
-          input.value = valor;
-        }
-
-        setIsLoading(false);
-      };
-
       Object.entries(dados).forEach(([tabela, campo]) => {
+        if (!campo) {
+          return;
+        }
+
         Object.entries(campo).forEach(([nome, valor]) => {
           if (tabela === "produtos") {
-            const id = { id: valor.id };
-            const produto = { [nome]: id };
-            const input = getProdutoInput(valor.produto);
-            tabelasId[tabela] = { ...tabelasId[tabela], ...produto };
-            preencherValor(input, valor);
+            const produto = valor.produto;
+            novoFormData[produto].valor = true;
           } else if (nome === "id") {
             const id = { id: valor };
             tabelasId[tabela] = id;
           } else {
-            const input = getInput(`${tabela}_${nome}`);
-            preencherValor(input, valor);
+            novoFormData[nome].valor = valor;
           }
         });
       });
 
-      console.log("Tabelas após preenchimento: ", tabelasId);
-      setListaId(tabelasId);
+      setFormData(novoFormData);
     }
 
     const ficha_link = sessionStorage.getItem("ficha_link");
 
     if (ficha_link) {
       preencherFicha(ficha_link);
+    } else {
+      setFormData(dadosForm);
     }
-  }, [formRef]);
+  }, [formData, setFormData]);
 
   return {
     pagina: {
