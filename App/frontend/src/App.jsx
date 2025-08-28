fd1787971d117a499e732d9d20a94aef6ccc8588
Luizxdas <luizxdas@outlook.com>
2025-08-28 12:51:50 -0300

    Adiciona configurações do Docker, configuração do nginx e corrige caminhos em App.jsx

--- App/frontend/src/App.jsx
+++ App/frontend/src/App.jsx
@@ -1,35 +1,20 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
-import Ficha from "./Componentes/Ficha";
-import Frente from "./Componentes/Frente/Frente";
-import Verso from "./Componentes/Verso/Verso";
-import Pacientes from "./Componentes/Pacientes/Pacientes";
+import Ficha from "./componentes/Ficha";
+import Frente from "./componentes/Frente/Frente";
+import Verso from "./componentes/Verso/Verso";
+import Pacientes from "./componentes/Pacientes/Pacientes";
 
 function App() {
-    return (
-        <Routes>
-            <Route
-                path="/"
-                element={<Home/>}
-            />
-            <Route
-                path="/ficha"
-                element={<Ficha/>}
-            />
-            <Route
-                path="/frente"
-                element={<Frente/>}
-            />
-            <Route
-                path="/verso"
-                element={<Verso/>}
-            />
-            <Route
-                path="/pacientes"
-                element={<Pacientes/>}
-            />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/ficha" element={<Ficha />} />
+      <Route path="/frente" element={<Frente />} />
+      <Route path="/verso" element={<Verso />} />
+      <Route path="/pacientes" element={<Pacientes />} />
+    </Routes>
+  );
 }
 
 export default App;
